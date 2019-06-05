1e7670d6fcd35422c06c2b4e9ba15e5cc2a707ab
Alpesh Choubisa <alpesh.choubisa@wirestone.com>
2019-06-05 11:14:59 +0530

    Singleton Design Pattern with Double Check

---
+++ Singleton with thread safe/Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using static System.Console;
+namespace Singleton
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Parallel.Invoke(() => LogManagerRequest(), () => LogEmployeeRequest());
+      ReadLine();
+    }
+
+    private static void LogManagerRequest()
+    {
+      Singleton fromManager = Singleton.SingleInstance;
+      fromManager.LogMessage("Request Message from Manager");
+    }
+    private static void LogEmployeeRequest()
+    {
+      Singleton fromEmployee = Singleton.SingleInstance;
+      fromEmployee.LogMessage("Request Message from Employee");
+    }
+  }
+}
---
+++ Singleton with thread safe/Singleton/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+namespace Singleton
+{
+  sealed class Singleton
+  {
+    static int instanceCounter = 0;
+    private static Singleton singleInstance = null;
+    private static readonly object lockObject = new object();
+    private Singleton()
+    {
+      instanceCounter++;
+      WriteLine("Instances created " + instanceCounter );
+    }
+
+    public static Singleton SingleInstance
+    {
+      get
+      {
+        if (singleInstance == null)
+        {
+          lock (lockObject)
+          {
+            if (singleInstance == null)
+            {
+              singleInstance = new Singleton();
+            }
+
+          }
+        }
+        return singleInstance;
+      }
+    }
+    public void LogMessage(string message)
+    {
+      WriteLine("Message " + message);
+    }
+  }
+}
